498d0a5cd55efff45e6213d46c7a267f4316adde
Rasen_San <fpfrancescopalma7@gmail.com>
2016-01-08 13:19:34 +0100

    Update accordion.js

--- Personal WebPage/accordion.js
+++ Personal WebPage/accordion.js
@@ -3,19 +3,18 @@
 /* global jQuery */
 jQuery(document).ready(function() {
 	function close_accordion_section() {
-		jQuery('.accordion .accordion-section-title').removeClass('active');
-		jQuery('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+        jQuery('.accordion .accordion-section-title').removeClass('active');
+		jQuery('.accordion .accordion-section-content').slideUp(500).removeClass('open');
 	}
 
 	jQuery('.accordion-section-title').click(function(e) {
 		var currentAttrValue = jQuery(this).attr('href');
-
-		if(jQuery(e.target).is('.active')) {
-			close_accordion_section();
-		} else {
-			close_accordion_section();
-			jQuery(this).addClass('active');
-			jQuery('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
+            if(jQuery(e.target).is('.active')) {
+			     close_accordion_section();
+		    } else {
+			     close_accordion_section();
+			     jQuery(this).addClass('active');
+			     jQuery('.accordion ' + currentAttrValue).slideDown(500).addClass('open'); 
 		}
 		e.preventDefault();
 	});
